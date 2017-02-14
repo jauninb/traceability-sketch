041c0f473b98f92d9028259fffa122e0699cff39
jaunin-b <jaunin.b@fr.ibm.com>
2017-02-14 11:37:36 +0100

    Doing some cleanup refactoring

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var port = (process.env.PORT || process.env.VCAP_APP_PORT || 7777);
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World!\n');
+	// Doing some cleanup - refactoring
+	var message = 'Hello World!\n';
+	res.end(message);
 }).listen(port);
 
