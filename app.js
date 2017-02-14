870c90adda48df5baab0747cda6bc3b101908c41
jaunin-b <jaunin.b@fr.ibm.com>
2017-02-14 11:34:52 +0100

    Change the port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var port = (process.env.PORT || process.env.VCAP_APP_PORT || 8888);
+var port = (process.env.PORT || process.env.VCAP_APP_PORT || 7777);
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
