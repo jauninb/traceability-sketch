ce9c8bceefd7aa7469856381e2d4d08b5e12c069
jaunin-b <jaunin.b@fr.ibm.com>
2017-02-14 11:36:39 +0100

    Change the port numbert to 7777 according to #2
    https://github.com/jauninb/traceability-sketch/issues/2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 
+// Change the port number according to issue #
 var port = (process.env.PORT || process.env.VCAP_APP_PORT || 7777);
 
 http.createServer(function (req, res) {
